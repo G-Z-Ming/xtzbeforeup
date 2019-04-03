09d1b93c26b7d317d168011a104e3b0b6be5b363
roger <1520196726@qq.com>
2019-04-03 22:56:47 +0800

    首页又再改一遍

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -7,18 +7,14 @@ export default {
       // 师资培训
       teachersList: [],
       JNheadline: [],
-      newHeaders: [
-      ],
+      newHeaders: [],
+      // 新闻tab切换用的index
       newsActiveIndex: 0,
       // 新闻
       newsDataList: [],
+      // 存储新闻的对象
+      newsStoreObj: {}
     };
-  },
-  // 时间过滤
-  filters: {
-    capitalize: function (value) {
-      return value.substring(0, 10)
-    }
   },
   template,
   // mounted () {
@@ -34,25 +30,48 @@ export default {
       // 获取新闻类型
       this.$ajax.get('xtz/portal/baseInfos', { params: { baseInfoId: '5', baseTypeId: '1' } }).then(rsp => {
         this.newHeaders = rsp.data.data
-      })
+      });
       // 获取新闻列表
-      this.$ajax.get('xtz/portal/informations', { params: { pageNum: '1', pageSize: '5', typeDId: '1', typeId: '5' } }).then(rsp => {
-        this.newsDataList = rsp.data.data
-      })
+      this.exchangeNews('1', 0);
       // 获取师资培训列表
       this.$ajax.get('xtz/portal/informations', { params: { pageNum: '1', pageSize: '5', typeId: '7' } }).then(rsp => {
-        this.teachersList = rsp.data.data
-      })
+        this.teachersList = rsp.data.data.map(item=>{
+          if(!!item.createtime){
+            item.createtime = this.capitalize(item.createtime)
+          }
+          return item;
+        });
+      });
+    },
+    /** 时间过滤
+     * @param value  type:string
+     **/
+    capitalize(value){
+      return value.substring(0, 10)
     },
     /**
     * 新闻报道的title点击事件
-    * @param index 索引
+    * @param index 索引  type: number
+    * @param id    id    type: string
     */
-    exchangeNews (index) {
-      this.$ajax.get('xtz/portal/informations', { params: { pageNum: '1', pageSize: '5', typeDId: index, typeId: '5' } }).then(rsp => {
-        this.newsDataList = rsp.data.data
-      })
-
+    exchangeNews (id, index) {
+      if(!!this.newsStoreObj[id]){
+        this.newsDataList = this.newsStoreObj[id];
+        this.newsActiveIndex = index;
+      }else{
+        this.$ajax.get('xtz/portal/informations', { params: { pageNum: '1', pageSize: '5', typeDId: id, typeId: '5' } }).then(rsp => {
+          this.newsDataList = rsp.data.data.map(item=>{
+            if(!!item.createtime){
+              item.createtime = this.capitalize(item.createtime)
+            }
+            return item;
+          });
+          this.newsStoreObj[id] = this.newsDataList;
+          this.newsActiveIndex = index;
+        }).catch(rej=>{
+          console.error(rej);
+        });
+      }
     },
   },
   created () {
