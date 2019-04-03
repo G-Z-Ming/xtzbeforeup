d76b49cde8a3b5318228e96691e7cff23b7f85dd
roger <1520196726@qq.com>
2019-04-04 00:07:04 +0800

    封装组件

---
+++ src/common/components/areaSelection/index.vue
@@ -0,0 +1,31 @@
+<script>
+  import template from './template.template';
+  export default {
+    data() {
+      return {
+        activeIndex: 0
+      }
+    },
+    props:{
+      title: '',
+      dataArr: {default:()=>[]}
+    },
+    template,
+    methods: {
+      /**
+       * 点击事件
+       * @param index
+       */
+      itemClick(index){
+        this.activeIndex = index;
+      }
+    },
+    created() {
+
+    }
+  }
+</script>
+<style lang="less" scoped>
+  @import "./style.less";
+
+</style>
