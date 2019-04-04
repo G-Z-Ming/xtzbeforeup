bc98e962cd7af3aa248d88dd2608d39aff2acacf
G-Z-Ming <13538014753@163.com>
2019-04-04 18:53:36 +0800

    解决冲突19.04.04 18:53

--- src/router/index.js
+++ src/router/index.js
@@ -10,108 +10,75 @@ import Identification from '@/components/identification/index' // 认证
 import Introduce from '@/components/introduce/index' //企业公司
 Vue.use(Router)
 let router = new Router({
-        routes: [{
-                path: '/',
-                redirect: 'Home'
-            },
-            {
-                path: '/login',
-                name: 'Login',
-                component: Login,
-            },
-            {
-                path: '/home',
-                name: 'Home',
-                component: Home,
-                meta: {
-                    auth: true
-                }
-            },
-            {
-                path: '/home/news',
-                name: 'News',
-                component: News,
-                meta: {
-                    auth: true
-                }
-            },
-            {
-                path: '/home/news/newsinfo',
-                name: 'NewsInfo',
-                component: NewsInfo,
-                meta: {
-                    auth: true
-                }
-            },
-            {
-                path: '/company',
-                name: 'Company',
-                component: Company,
-                meta: {
-                    auth: true
-                }
-            },
-            {
-                path: '/school',
-                name: 'School',
-                component: School,
-                meta: {
-                    auth: true
-                }
-            },
-
-            {
-                path: '/identification',
-                name: 'Identification',
-                component: Identification,
-                meta: {
-                    auth: true
-                }
-            }
-        ]
-    })
-    /*router.beforeEach((to, from, next) => {
-      if (to.matched.some(record => record.meta.auth)){  // 判断该路由是否需要登录权限
-        if (localStorage.getItem("token")) {  // 判断当前的token是否存在
-          next();
-        }
-        else {
-          next({
+    routes: [{
+            path: '/',
+            redirect: 'Home'
+        },
+        {
             path: '/login',
-          })
+            name: 'Login',
+            component: Login,
+        },
+        {
+            path: '/home',
+            name: 'Home',
+            component: Home,
+            meta: {
+                auth: true
+            }
+        },
+        {
+            path: '/home/news',
+            name: 'News',
+            component: News,
+            meta: {
+                auth: true
+            }
+        },
+        {
+            path: '/home/news/newsinfo',
+            name: 'NewsInfo',
+            component: NewsInfo,
+            meta: {
+                auth: true
+            }
+        },
+        {
+            path: '/company',
+            name: 'Company',
+            component: Company,
+            meta: {
+                auth: true
+            }
+        },
+        {
+            path: '/introduce',
+            name: 'Introduce',
+            component: Introduce,
+            meta: {
+                auth: true
+            }
+        },
+        {
+            path: '/school',
+            name: 'School',
+            component: School,
+            meta: {
+                auth: true
+            }
+        },
+        {
+            path: '/identification',
+            name: 'Identification',
+            component: Identification,
+            meta: {
+                auth: true
+            }
         }
-      }
-      else {
-        next();
-      }
-<<<<<<< HEAD
-    },
-    {
-      path:'/introduce',
-      name:'Introduce',
-      component:Introduce,
-      meta:{
-        auth:true
-      }
-    },
-    {
-      path: '/school',
-      name: 'School',
-      component: School,
-      meta:{
-        auth:true
-      }
-    },
-    {
-      path: '/identification',
-      name: 'Identification',
-      component: Identification,
-      meta:{
-        auth:true
-      }
-    }
-  ]
+
+    ]
 })
+
 /*router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.auth)){  // 判断该路由是否需要登录权限
     if (localStorage.getItem("token")) {  // 判断当前的token是否存在
@@ -128,7 +95,3 @@ let router = new Router({
   }
 });*/
 export default router
-=======
-    });*/
-export default router
->>>>>>> 0d79ce80398e9bdde1d11dfb10357b3ea0525ed8
