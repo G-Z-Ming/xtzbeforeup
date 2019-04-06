de6c97d1df9a609cbd72cbc6e4db7990ac368c4b
lennon <lennon2013@163.com>
2019-04-06 22:38:45 +0800

    剩余页面提交

---
+++ src/components/detailed/index.vue
@@ -0,0 +1,19 @@
+<script>
+import template from './template.template';
+export default {
+  name: 'detailed',
+  data () {
+    return {
+    }
+  },
+  template,
+  created () {
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang="less" scoped>
+@import './style.less';
+</style>
---
+++ src/components/recruit/index.vue
@@ -0,0 +1,30 @@
+<script>
+import template from './template.template';
+export default {
+  name: 'recruit',
+  data () {
+    return {
+      navName:[
+        {name:'全部',id:0},
+        {name:'学徒招聘',id:1},
+        {name:'内部招聘',id:2},
+        {name:'兼职招聘',id:3}
+      ]
+    }
+  },
+  template,
+  created () {
+  },
+  methods: {
+// 点击单独的跳转到详情页
+jump(){
+  console.log('5555');
+  
+  this.$router.push({path:'position'})
+}
+  }
+}
+</script>
+<style lang="less" scoped>
+@import './style.less';
+</style>
