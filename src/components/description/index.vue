5bdbc4500e4ae47c83b109ac2ff39d57e9ac5276
Front-biger <626497248@qq.com>
2019-04-08 10:43:09 +0800

    职业描述+课程体系

---
+++ src/components/description/index.vue
@@ -0,0 +1,16 @@
+<script>
+import template from "./template.template";
+export default {
+  name: "description",
+  data() {
+    return {
+    };
+  },
+  template,
+  methods: {
+  }
+};
+</script>
+<style lang="less" scoped>
+@import "./style.less";
+</style>
---
+++ src/components/system/index.vue
@@ -0,0 +1,16 @@
+<script>
+import template from "./template.template";
+export default {
+  name: "system",
+  data() {
+    return {
+    };
+  },
+  template,
+  methods: {
+  }
+};
+</script>
+<style lang="less" scoped>
+@import "./style.less";
+</style>
