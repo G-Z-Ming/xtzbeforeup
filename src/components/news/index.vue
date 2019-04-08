a719bde06e85a813ba8f8e0204a268c876bf21c2
G-Z-Ming <13538014753@163.com>
2019-04-08 17:53:16 +0800

    新闻列表页修改19-04-08-17:52

--- src/components/news/index.vue
+++ src/components/news/index.vue
@@ -4,11 +4,34 @@ export default {
   name: "news",
   data() {
     return {
-      text: "新闻列表"
+      // text: "新闻列表"
+      newsListData: [],
     };
   },
   template,
+  created(){
+    this.init();
+  },
   methods: {
+    init(){
+      this.$ajax.get('xtz/portal/informations',{params:{pageNum: '1', pageSize: '5', typeDId: '2', typeId: '5'}}).then(rsp=>{
+        console.log(rsp.data.data);
+        // let rspData = rsp.data;
+        // console.log(rspData.data);
+        this.newsListData = rsp.data.data.map(item=>{
+          if(!!item.createtime){
+            item.createtime = this.capitalize(item.createtime)
+          }
+          return item;
+        })
+      });
+    },
+    /** 时间过滤
+     * @param value  type:string
+     **/
+    capitalize(value){
+      return value.substring(0, 10)
+    },
     gotoNewsInfo(){
       this.$router.push({
         path: "/home/news/newsinfo"
