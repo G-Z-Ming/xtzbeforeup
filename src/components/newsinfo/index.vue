a2911db36e465ab8a2bcb74a3af2702f8b953a6d
luojiexiang <1520196726@qq.com>
2019-04-09 11:26:42 +0800

    newsInfo调通接口

--- src/components/newsinfo/index.vue
+++ src/components/newsinfo/index.vue
@@ -10,12 +10,29 @@
       };
     },
     template,
-    methods: {},
+    methods: {
+      dateFilter(input, splitchar){
+        var input = new Date(input);
+        var y = input.getFullYear();
+        var m = input.getMonth() + 1;
+        var d = input.getDate();
+        var h = input.getHours();
+        var mi = input.getMinutes();
+        var s = input.getSeconds();
+        if (splitchar == "yyyy-MM-dd hh:mm") {
+          return y + "-" + m + "-" + d + " " + h + ":" + mi;
+        }
+        if (splitchar == "YY-MM-DD") {
+          return y + "-" + m + "-" + d;
+        }
+      }
+    },
     created() {
       // 获取新闻id
       const {id} = this.$route.query;
       // 获取接口
       this.$ajax.get('xtz/portal/information', {params: {informationId: id}}).then(rsp => {
+        rsp.data.data.createtime = this.dateFilter(rsp.data.data.createtime, 'yyyy-MM-dd hh:mm');
         this.newsInfo = rsp.data.data;
       }).catch(rej => {
         this.newsInfo = {};
